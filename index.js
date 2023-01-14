69e8a229fd6d8192733a145ec147ea36e62dbb71
srajankumar <kumarsrajan02@gmail.com>
2023-01-14 11:58:27 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send("Hello World!...");
+});
+
 app.listen(3000, function () {
   console.log("Server is up and running...");
 });
